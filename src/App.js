d3da546364da1deb331b0aa878275d45e4aa5dc7
nayyyhaa <nehaguptang.ng@gmail.com>
2021-09-02 00:08:29 +0530

    feat: display on next

--- src/App.js
+++ src/App.js
@@ -7,34 +7,41 @@ function App() {
   let [returnedAmt, setReturnedAmt] = useState();
   let [errorMsg, setErrorMsg] = useState('');
   let [showError, setShowError] = useState(false);
+  let [showChangeAmtContent, setShowChangeAmtContent] = useState(false);
   let [changeArr, setChangeArr] = useState([0,0,0,0,0,0,0])
   let cashArray = [2000, 500, 100, 20, 10, 5, 1];
+  let changeAmt;
 
   function calculateChangeAmt() {
-    let changeAmt = cashGivenAmt - billAmt;
-    if(changeAmt > 0) {
+    changeAmt = cashGivenAmt - billAmt;
+    if(changeAmt > 0 && billAmt>0) {
       setShowError(false);
       setReturnedAmt(changeAmt);
-      calculateNoteChange(changeAmt);
+      calculateNoteChange();
     }
-    else if(changeAmt < 0) {
+    else {
       setShowError(true);
-      setErrorMsg("Cash Given value is less than bill, Please Enter right Amount");
+      if(changeAmt < 0) setErrorMsg("Cash Given value is less than bill, Please Enter right Amount");
+      else if(changeAmt === 0) setErrorMsg("No Cash to be returned!");
+      else setErrorMsg("Enter Valid Amount Please!");
     }
-    else if(changeAmt === 0) {
-      setShowError(true);
-      setErrorMsg("No Cash to be returned!");
-    }
-    else{
-      setShowError(true);
-      setErrorMsg("Enter Valid Amount Please!");
-    } 
   }
 
-  function calculateNoteChange(changeAmt) {
-    for(let i=0;i<7;i++) {
+  function validateBillAmt() {
+    if(billAmt>0) {
+      setShowError(false);
+      setShowChangeAmtContent(true);
+    }
+    else {
+      setShowError(true);
+      setErrorMsg("Enter Valid Amount Please!");
+    }
+  }
+
+  function calculateNoteChange() {
+    for(let i=0; i<cashArray.length; i++) {
       let noOfNotes = Math.floor(changeAmt / cashArray[i]);
-      if(noOfNotes > 0 && i<7) {
+      if(noOfNotes > 0) {
         changeAmt = changeAmt % cashArray[i];
         changeArr[i] = noOfNotes;
       }
@@ -53,11 +60,14 @@ function App() {
         <div className="box-content">
           <label htmlFor="bill-amount">Total Bill Amount</label>
           <input id="bill-amount" type="number" onChange={(e) => setBillAmt(e.target.value)}/>
-          <label htmlFor="cash-given">Cash Given</label>
-          <input id="cash-given" type="number"onChange={(e) => setCashGivenAmt(e.target.value)}/>
-          <button className="check" onClick={calculateChangeAmt}>check</button>
+          <button onClick={validateBillAmt} style={!showChangeAmtContent? {display: 'flex'}: {display: 'none'}}>Next</button>
+          <div className="cash-given-content" style={showChangeAmtContent? {display: 'flex'}: {display: 'none'}}>
+            <label htmlFor="cash-given">Cash Given</label>
+            <input id="cash-given" type="number" onChange={(e) => setCashGivenAmt(e.target.value)}/>
+            <button className="check" onClick={calculateChangeAmt}>check</button>
+          </div>
           <p className="errorMsg" style={showError? {display: 'block'}: {display: 'none'}}>{errorMsg}</p>
-          <div className="returnTable" style={!showError? {display: 'block'}: {display: 'none'}}>
+          <div className="returnTable" style={!showError && returnedAmt>0? {display: 'block'}: {display: 'none'}}>
             <p>Total amount to be returned: {returnedAmt}</p>
             <table className="cash-table">
               <tbody>
