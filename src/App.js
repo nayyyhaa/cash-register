3ce1066e9bcb1ec7fb920324771f7d5a401283f0
nayyyhaa <nehaguptang.ng@gmail.com>
2021-09-01 18:17:09 +0530

    inital setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  let [billAmt, setBillAmt] = useState();
+  let [cashGivenAmt, setCashGivenAmt] = useState();
+  let [returnedAmt, setReturnedAmt] = useState();
+  let [errorMsg, setErrorMsg] = useState('');
+  let [showError, setShowError] = useState(false);
+  let [changeArr, setChangeArr] = useState([0,0,0,0,0,0,0])
+  let cashArray = [2000, 500, 100, 20, 10, 5, 1];
+
+  function calculateChangeAmt() {
+    let changeAmt = cashGivenAmt - billAmt;
+    if(changeAmt > 0) {
+      setShowError(false);
+      setReturnedAmt(changeAmt);
+      calculateNoteChange(changeAmt);
+    }
+    else if(changeAmt < 0) {
+      setShowError(true);
+      setErrorMsg("Cash Given value is less than bill, Please Enter right Amount");
+    }
+    else if(changeAmt === 0) {
+      setShowError(true);
+      setErrorMsg("No Cash to be returned!");
+    }
+    else{
+      setShowError(true);
+      setErrorMsg("Enter Valid Amount Please!");
+    } 
+  }
+
+  function calculateNoteChange(changeAmt) {
+    for(let i=0;i<7;i++) {
+      let noOfNotes = Math.floor(changeAmt / cashArray[i]);
+      if(noOfNotes > 0 && i<7) {
+        changeAmt = changeAmt % cashArray[i];
+        changeArr[i] = noOfNotes;
+      }
+      else changeArr[i] = 0;
+    }
+    setChangeArr([...changeArr])
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <header>
+          <h1>Cash Register Manager</h1>
+        </header>
+        <p className="description">Enter the bill amount and cash given by the customer and know the minimum number of notes to return.</p>
+        <div className="box-content">
+          <label htmlFor="bill-amount">Total Bill Amount</label>
+          <input id="bill-amount" type="number" onChange={(e) => setBillAmt(e.target.value)}/>
+          <label htmlFor="cash-given">Cash Given</label>
+          <input id="cash-given" type="number"onChange={(e) => setCashGivenAmt(e.target.value)}/>
+          <button className="check" onClick={calculateChangeAmt}>check</button>
+          <p className="errorMsg" style={showError? {display: 'block'}: {display: 'none'}}>{errorMsg}</p>
+          <div className="returnTable" style={!showError? {display: 'block'}: {display: 'none'}}>
+            <p>Total amount to be returned: {returnedAmt}</p>
+            <table className="cash-table">
+              <tbody>
+                <tr>
+                  <th>No. of Notes</th>
+                  {changeArr.map((arr, index) => {
+                    return <td key={index}>{arr}</td>
+                  })}
+                </tr>
+                <tr>
+                  <th>Note</th>
+                  <td>2000</td>
+                  <td>500</td>
+                  <td>100</td>
+                  <td>20</td>
+                  <td>10</td>
+                  <td>5</td>
+                  <td>1</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
